89d1a2c1c6254073b552578047b7093a40a4952a
Eric Lavachery <eric.lavachery@gmail.com>
2020-02-24 21:18:59 +0100

    dbconnect out

--- modules/dbconnect_local.js
+++
@@ -1,9 +0,0 @@
-// connect to mysql
-var mysql = require('mysql');
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "kqb",
-    password: "zen8070$mysql",
-    database: "mpg"
-});
-module.exports.con = con;
