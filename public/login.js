e432ad1813fa6b1ed5eee7094cd6257b9beed745
Eric Lavachery <eric.lavachery@gmail.com>
2020-02-29 13:42:18 +0100

    let's go

--- public/login.js
+++ public/login.js
@@ -1,11 +1,16 @@
 // Connexion à socket.io
 var socket = io.connect('http://localhost:8080', {'sync disconnect on unload': true});
+
+// LOGIN ---------------------------------------------------------------
+// fonction pour Connexion
+
 let pseudo = new URLSearchParams(document.location.search).get("pseudo");
 if (pseudo != '' && pseudo != null && pseudo != undefined) {
     connectMe(pseudo);
 } else {
     pseudo = 'anonymous';
 }
+
 function promptMe() {
     // On demande le pseudo, on l'envoie au serveur
     pseudo = prompt('Quel est votre pseudo ?');
@@ -17,4 +22,19 @@ function promptMe() {
 }
 function connectMe(pseudo) {
     socket.emit('newcli', pseudo);
+    // montre tout sauf le bouton connexion
+    document.getElementById('con').style.display = 'block';
+    document.getElementById('pascon').style.display = 'none';
+    // change le titre du doc
+    document.title = 'Starship Miners - ' + pseudo;
 }
+
+$('#hello').click(promptMe);
+
+// $('#medit').click(function() {
+//     window.location = "/edit/?pseudo=" + pseudo;
+// });
+//
+// $('#home').click(function() {
+//     window.location = "/?pseudo=" + pseudo;
+// });
