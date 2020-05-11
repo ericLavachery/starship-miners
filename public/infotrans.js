0e2d5fa20a4c46e3ecfeeff9663c18e8fa903ac4
Eric Lavachery <eric.lavachery@gmail.com>
2020-05-11 18:13:51 +0200

    units volume

--- public/infotrans.js
+++ public/infotrans.js
@@ -32,7 +32,7 @@ function unloadInfos(myBat,myBatUnitType) {
 };
 
 function calcVolume(batType) {
-    return Math.round(batType.size*batType.squadSize*batType.squads/1.7*Math.sqrt(batType.size));
+    return Math.round(batType.size*batType.squadSize*batType.squads/3*Math.sqrt(batType.size+6)*batType.volume);
 };
 
 function checkTransportId(myBat,myBatType) {
