f0eec635670fdbcead11b76938f47ff0cabaeca7
Eric Lavachery <eric.lavachery@gmail.com>
2021-03-13 02:51:24 +0100

    stuff

--- public/fire.js
+++ public/fire.js
@@ -946,6 +946,7 @@ function attack(melee) {
                 if (totalDamage >= 10+(playerInfos.comp.ca*2)) {
                     targetBat.tags.push('necro');
                     targetBat.tags.push('necro');
+                    targetBat.tags.push('necro');
                     console.log('Nécrotoxine!');
                     $('#report').append('<span class="report rose">Nécrotoxine<br></span>');
                 }
@@ -1578,6 +1579,7 @@ function defense(melee) {
         if (!selectedBat.tags.includes('necro')) {
             if (selectedBatType.cat == 'infantry' && !selectedBat.tags.includes('zombie')) {
                 if (totalDamage >= 10+(playerInfos.comp.ca*2)) {
+                    selectedBat.tags.push('necro');
                     selectedBat.tags.push('necro');
                     selectedBat.tags.push('necro');
                     console.log('Nécrotoxine!');
