d054ab2547c0fdcb1b491c9d773f01445f7def86
Eric Lavachery <eric.lavachery@gmail.com>
2021-06-12 11:28:46 +0200

    penitbat

--- public/nextweek.js
+++ public/nextweek.js
@@ -293,12 +293,16 @@ function calcCrimeRate(mesCitoyens) {
     // Unités: (electroguards-2 gurus-2 dealers-1 marshalls-1)
     let maxAntiCrimeUnits = Math.round(Math.sqrt(population)/7.5);
     let antiCrimeUnits = 0;
+    let penitBatCrims = 0;
     // Bâtiments: (prisons-5 salleSport-2 jardin-4 bar-2 cantine-3 dortoirs+1 cabines-1 appartements+3)
     let sortedBatList = bataillons.slice();
     sortedBatList = _.sortBy(sortedBatList,'sort');
     sortedBatList.reverse();
     sortedBatList.forEach(function(bat) {
         let batType = getBatType(bat);
+        if (batType.skills.includes('penitbat')) {
+            penitBatCrims = penitBatCrims+(batType.squads*batType.crew*batType.squadSize);
+        }
         if (batType.name === 'Dortoirs') {
             lits = lits+500;
         }
@@ -349,13 +353,26 @@ function calcCrimeRate(mesCitoyens) {
     if (crimeRate.penib < 0) {
         crimeRate.penib = 0;
     }
+    // Bataillons pénitentiaires
+    let penitAdj = 0;
+    let adjCrims = crimeRate.crim;
+    if (playerInfos.comp.ordre === 1) {
+        penitAdj = Math.round(penitBatCrims*2/9);
+    } else if (playerInfos.comp.ordre === 2) {
+        penitAdj = Math.round(penitBatCrims*4/9);
+    } else if (playerInfos.comp.ordre === 3) {
+        penitAdj = Math.round(penitBatCrims*6/9);
+    }
+    adjCrims = Math.ceil((mesCitoyens.crim-penitAdj)*100/population);
+    console.log('crimeRate.crim='+crimeRate.crim);
+    console.log('adjCrims='+adjCrims);
     // Treshold
     if (crimeRate.penib < 10) {
-        crimeRate.total = crimeRate.crim+Math.round(crimeRate.penib/1.9);
-    } else if (crimeRate.crim >= 15) {
-        crimeRate.total = crimeRate.crim+Math.round(crimeRate.penib*1.2);
+        crimeRate.total = adjCrims+Math.round(crimeRate.penib/1.9);
+    } else if (adjCrims >= 15) {
+        crimeRate.total = adjCrims+Math.round(crimeRate.penib*1.2);
     } else {
-        crimeRate.total = crimeRate.crim+crimeRate.penib;
+        crimeRate.total = adjCrims+crimeRate.penib;
     }
     crimeRate.total = crimeRate.total+crimeRate.fo;
     // compétence maintien de l'ordre
